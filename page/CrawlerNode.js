69c930eff2173325cd656f411c3408380b85fbd0
Xavier Blanc <xavier.blanc@labri.fr>
2017-07-06 18:11:44 +0200

    bugfix

--- page/CrawlerNode.js
+++ page/CrawlerNode.js
@@ -6,10 +6,10 @@ class CrawlerNode {
         this.siteId = siteId;
         this.serverNames = serverNames;
         this.sftpClient = new SftpClient({
-            var host = 'localhost';
-            var port = '2222';
-            var username = 'mrssam';
-            var password = 'mrssam';
+            host : 'localhost';
+            port : '2222';
+            username : 'mrssam';
+            password : 'mrssam';
         });
         this.dbUrl = `mongodb://${serverNames.mongoServerName}:27017/mrs-sam-page`;
         this.rmqUrl = `amqp://${serverNames.rabbitServerName}`;
