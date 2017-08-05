0d14458d4ded62848f8389f4a24de813ea1aafbb
Thomas Durieux <tdurieux@users.noreply.github.com>
2017-08-05 17:59:45 +0200

    Update run.sh

--- scenario/docker/run.sh
+++ scenario/docker/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mkdir work
 cd work
-git clone https://github.com/xblanc33/mrs-sam.git
+git clone https://github.com/jrfaller/mrs-sam.git
 ls 
-cd mrs-sam
+cd mrs-sam/scenario
 npm install
 xvfb-run --server-args="-screen 0 1024x768x24" node server.js mongo
